5e4794308e54148c78b08a32d49dbad179a1f0bd
duchoa117 <duchoapc99@gmail.com>
2018-04-07 17:35:59 +0700

    <3

--- wed3/optional_homework/app.py
+++ wed3/optional_homework/app.py
@@ -1,17 +1,20 @@
 from flask import *
 app = Flask(__name__)
-from pymongo import MongoClient
-MongoClient_uri = 'mongodb://admin:admin@ds021182.mlab.com:21182/c4e'
-client = MongoClient(MongoClient_uri)
-db = client.get_default_database()
-river = db['river']
-# all_rivers = rivers.find()
+from models.c4e import River
+import mlab
+mlab.connect()
 
 
 @app.route('/')
 def river_table():
-    all_rivers = river.find()
-    return render_template('river_table.html', all_rivers = all_rivers)
+
+    all_rivers = River.objects(continent = "Africa")
+    return render_template('afica_river_table.html', all_rivers = all_rivers)
+@app.route('/ALL_rivers_in_‘S. America’_continent_with_length_less_than_1000_km')
+def America_lt_100_river():
+    all_rivers = River.objects(continent = 'S. America',length__lt = 1000)
+    return render_template('a_river.html', all_rivers = all_rivers)
+
 
 if __name__ == '__main__':
   app.run(debug=True)
